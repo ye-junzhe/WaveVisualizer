ebf4b47672bacefbc3d1c4aedfd0c14f31d93091
Ye Junzhe <junzheyez@gmail.com>
2023-08-05 21:11:54 +0800

    FFS integrated, but not working properly

--- WaveVisualizer.c
+++ WaveVisualizer.c
@@ -1,8 +1,12 @@
 #include <assert.h>
+#include <complex.h>
+#include <math.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "fft.c"
 
 #include "/opt/homebrew/Cellar/raylib/4.5.0/include/raylib.h"
 
@@ -13,38 +17,62 @@ typedef struct {
     float right;
 } Frame;
 
-Frame global_frames[4800*2] = {0};
-size_t global_frames_count = 0;
+#define N 64
+float in[N];
+float complex out[N];
+float max_amp;
+
+float amp(float complex z) {
+    float a = fabsf(crealf(z));
+    float b = fabsf(cimagf(z));
+    if (a < b) return b;
+    return a;
+}
+
 
 // Param1: Buffer holding the data
 // Param2: The data we send into the buffer
 void callback(void *bufferData, unsigned int frames)
 {
-    size_t capacity = ARRAY_LEN(global_frames);
-    // First scenario: the frames can fit into the remaining memory
-    if (frames <= capacity - global_frames_count) {
-        memcpy(global_frames + global_frames_count, bufferData, sizeof(Frame)*frames);
-        global_frames_count += frames;
-        return;
-    }
-    // Second scenario: the frames can't fit into the remaining memory,
-    // so we need to move the memory after the frames' end address to the start address of the buffer
-    // and then copy the new frames to where the frames' end address located in the buffer after the move
-    else if (frames <= capacity) {
-        memmove(global_frames, global_frames + sizeof(Frame)*frames, sizeof(Frame)*(capacity - frames));
-        memcpy(global_frames + (capacity - frames), bufferData, sizeof(Frame)*frames);
-    }
-    // Third scenario: the frames are larger then the bufferData
-    // Only cpy the buffer-sized of the frames into the memory
-    else {
-        memcpy(global_frames, bufferData, sizeof(Frame)*capacity);
-        global_frames_count = capacity;
+    // size_t capacity = ARRAY_LEN(global_frames);
+    // // First scenario: the frames can fit into the remaining memory
+    // if (frames <= capacity - global_frames_count) {
+    //     memcpy(global_frames + global_frames_count, bufferData, sizeof(Frame)*frames);
+    //     global_frames_count += frames;
+    //     return;
+    // }
+    // // Second scenario: the frames can't fit into the remaining memory,
+    // // so we need to move the memory after the frames' end address to the start address of the buffer
+    // // and then copy the new frames to where the frames' end address located in the buffer after the move
+    // else if (frames <= capacity) {
+    //     memmove(global_frames, global_frames + sizeof(Frame)*frames, sizeof(Frame)*(capacity - frames));
+    //     memcpy(global_frames + (capacity - frames), bufferData, sizeof(Frame)*frames);
+    // }
+    // // Third scenario: the frames are larger then the bufferData
+    // // Only cpy the buffer-sized of the frames into the memory
+    // else {
+    //     memcpy(global_frames, bufferData, sizeof(Frame)*capacity);
+    //     global_frames_count = capacity;
+    // }
+    //
+    // if (frames > ARRAY_LEN(global_frames)) frames = ARRAY_LEN(global_frames);
+    //
+    // memcpy(global_frames, bufferData, sizeof(Frame)*frames);
+    // global_frames_count = frames;
+    if (frames < N) return;
+
+    Frame *fs = bufferData;
+    for (size_t i = 0; i < frames; ++i) {
+        in[i] = fs[i].left;
     }
 
-    if (frames > ARRAY_LEN(global_frames)) frames = ARRAY_LEN(global_frames);
+    fft(in, 1, out, N);
 
-    memcpy(global_frames, bufferData, sizeof(Frame)*frames);
-    global_frames_count = frames;
+    max_amp = 0.0f;
+    for (size_t i = 0; i < frames; ++i) {
+        float a = amp(out[i]);
+        if (max_amp < a) max_amp = a;
+    }
 }
 
 int main(void)
@@ -81,15 +109,19 @@ int main(void)
 
         BeginDrawing();
         ClearBackground(CLITERAL(Color) {0x18, 0x18, 0x18, 0xFF});
-        float cell_width = (float)w/global_frames_count; 
-        for (size_t i = 0; i < global_frames_count; ++i) {
-            float t = global_frames[i].left;
-            if (t > 0) {
-                DrawRectangle(i*cell_width, h/2 - h/2*t, 1, h/2*t, RED);
-            } else {
-                DrawRectangle(i*cell_width, h/2, 1, h/2*t, RED);
-            }
+        float cell_width = (float)w/N; 
+        for (size_t i = 0; i < N; ++i) {
+            float t = amp(out[i]);//max_amp;
+            DrawRectangle(i*cell_width, h/2 - h/2*t, cell_width, h/2*t, RED);
         }
+        // for (size_t i = 0; i < global_frames_count; ++i) {
+        //     float t = global_frames[i].left;
+        //     if (t > 0) {
+        //         DrawRectangle(i*cell_width, h/2 - h/2*t, 1, h/2*t, RED);
+        //     } else {
+        //         DrawRectangle(i*cell_width, h/2, 1, h/2*t, RED);
+        //     }
+        // }
         EndDrawing();
     }
     return 0;
--- fft.c
+++ fft.c
@@ -2,41 +2,115 @@
 #include <math.h>
 #include <stdio.h>
 #include <complex.h>
+#include <assert.h>
 
 float pi;
 
-int main()
-{
-    pi = acosf(-1);
-
-    size_t n = 8;
-    float in[n]; // n samples
-    float complex out[n]; // n frequences we measure
-
-    for (size_t i = 0; i < n; ++i) {
-        float t = (float)i/n;
-        // Where waves have acoustic interference
-        in[i] = cosf(2*pi*t*1) + sinf(2*pi*t*2); // 1 & 2 Hertz
-    }
-
-    // for (size_t i = 0; i < n; i++) {
-    //     printf("%f\n", in[i]);
-    // }
-    // printf("\n");
-
+// NOTE: DFT
+void dft(float in[], float complex out[], size_t n) {
     for (size_t f = 0; f < n; ++f) {
         out[f] = 0;
         for (size_t i = 0; i < n; ++i) {
             float t= (float)i/n;
             // If the frequence is part of the input wave,
-            // then the output should be bigger than 0
+            // then the output should be upper 0
             out[f] += in[i]*cexp(2*I*pi*f*t);
         }
     }
+}
 
-    for (size_t f = 0; f < n; f++) {
-        printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
+// NOTE: FFT
+void fft(float in[], size_t stride, float complex out[], size_t n) {
+
+    assert(n > 0);
+    if (n == 1) {
+        out[0] = in[0];
+        return;
     }
 
-    return 0;
+    fft(in, stride*2, out, n/2);
+    fft(in + stride, stride*2, out + n/2, n/2);
+
+    for (size_t k = 0; k < n/2; ++k) {
+        float t = (float)k/n;
+        float complex v = cexp(-2*I*pi*t)*out[k + n/2];
+        float complex e = out[k];
+        out[k]       = e + v;
+        out[k + n/2] = e - v;
+    }
 }
+
+// int main()
+// {
+//     pi = acosf(-1);
+//
+//     size_t n = 8;
+//     float in[n]; // n samples
+//     float complex out[n]; // n frequences we measure
+//
+//     for (size_t i = 0; i < n; ++i) {
+//         float t = (float)i/n;
+//         // Where waves have acoustic interference
+//         in[i] = cosf(2*pi*t*1) + sinf(2*pi*t*2); // 1 & 2 Hertz
+//     }
+//
+//     // dft(in, out, n);
+//     fft(in, 1, out, n);
+//
+// // WARNING:
+// #if 0
+//
+//     /* NOTE: FFT
+//         * * * * * * * * | * * * * * * * *
+//         In FFT we only consider the first half of the n frequences
+//         Due to the periodicity of the sin and cos waves:
+//             All of the odd&even frequences have the same value at some point
+//             For each pair of frequences, the absolute value are the same
+//      */
+//
+//     for (size_t f = 0; f < n/2; ++f) {
+//         out[f] = 0;
+//         out[f + n/2] = 0;
+//         // even
+//         for (size_t i = 0; i < n; i += 2) {
+//             float t = (float)i/n;
+//             float complex v = in[i]*cexp(2*I*pi*t*f);
+//             out[f] += v;
+//             out[f + n/2] += v;
+//         }
+//         // odd
+//         for (size_t i = 1; i < n; i += 2) {
+//             float t = (float)i/n;
+//             float complex v = in[i]*cexp(2*I*pi*t*f);
+//             out[f] += v;
+//             out[f + n/2] -= v;
+//         }
+//     }
+//
+//     /* NOTE: List the samples for each frequences
+//
+//     size_t f = 1;
+//     // f += n/2;
+//     printf("%02zu:", f);
+//     for (size_t i = 1; i < n; i += 2) {
+//         float t = (float)i/n;
+//         printf(" %5.2f", sinf(2*pi*t*f));
+//     }
+//     printf("\n");
+//     */
+//
+//     /* NOTE: List the input sample
+//         for (size_t i = 0; i < n; i++) {
+//             printf("%f\n", in[i]);
+//         }
+//         printf("\n");
+//     */
+//
+// #endif
+//
+//     for (size_t f = 0; f < n; f++) {
+//         printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
+//     }
+//
+//     return 0;
+// }
